63e5898d767dc10be961cc868a56f1e9e4a86cdf
Josel099 <joseltech2002@gmail.com>
2023-02-05 23:30:39 +0530

    directly the text in the inputLabel is concatinated with the seven

--- src/project/Calculator.java
+++ src/project/Calculator.java
@@ -182,9 +182,8 @@ public class Calculator implements ActionListener{           // interface implen
 		//when the user clicks the button automatically call the interface
 				// downwards , action specifying when button is clicked // display the button name when button pressed 
 		if(e.getSource()==sevenButton){		// e.getSource used to check the whick button is clicked 
-				String labelText =displayLabel.getText();
-			
-			displayLabel.setText(labelText+"7");
+																								//String labelText =displayLabel.getText();
+				displayLabel.setText(displayLabel.getText()+"7");											//displayLabel.setText(labelText+"7");
 			}
 		 else if  (e.getSource()==eightButton){
 			displayLabel.setText("8");
